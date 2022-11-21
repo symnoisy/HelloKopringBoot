e0a733a546cc512d96a0e3b1ec35e51abc7aabef
symnoisy <symnoisy@gmail.com>
2022-11-21 16:00:52 +0900

    feat: Swagger 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.9")  // Swagger:SpringDoc
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9") // Swagger:SpringDoc
+
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
