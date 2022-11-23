b281737460cc4c1a31e4af405a3cfa86bbe01c6f
symnoisy <symnoisy@gmail.com>
2022-11-23 19:23:01 +0900

    feat: yahooFinance data parse 구현 완료

--- src/main/kotlin/com/hellokopringboot/demo/api/v1/StockParseController.kt
+++ src/main/kotlin/com/hellokopringboot/demo/api/v1/StockParseController.kt
@@ -1,11 +1,16 @@
 package com.hellokopringboot.demo.api.v1
 
+import com.fasterxml.jackson.module.kotlin.jsonMapper
+import io.swagger.v3.core.util.Json
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 
 @Tag(name="Stock Data Parse Controller")
@@ -13,15 +18,20 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/stock")
 class StockParseController {
 
-    @GetMapping("{ticker}.KS")
+    @PostMapping("")
     fun getStockInfoList(
-        @PathVariable(name = "ticker") ticker: Long,
-        @RequestParam("interval", defaultValue = "5") interval: Int,
-        @RequestParam("range", defaultValue = "5") range: Int,
-    ): String {
-        val helloWorld = "Hello World"
+        @RequestParam("ticker", defaultValue = "005930.KS") ticker: String,
+        @RequestParam("interval", defaultValue = "1d") interval: String,
+        @RequestParam("range", defaultValue = "5d") range: String,
+    ): String? {
+        // YahooFinance Documentation: https://syncwith.com/yahoo-finance/yahoo-finance-api
 
-        return helloWorld
+        val client = HttpClient.newBuilder().build()
+        val yahooFinanceURI = "https://query1.finance.yahoo.com/v8/finance/chart/$ticker?interval=$interval&range=$range"
+        val request = HttpRequest.newBuilder().uri(URI.create(yahooFinanceURI)).build()
+        val responseBody = client.send(request, HttpResponse.BodyHandlers.ofString()).body()
+
+        return responseBody
     }
 
 }
