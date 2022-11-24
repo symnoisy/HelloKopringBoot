2ecc47a1ff66721fb9c412540d32c0bfd74c2292
symnoisy <symnoisy@gmail.com>
2022-11-25 01:20:31 +0900

    feat: 요구사항 명세 Response Spec. 반영

--- src/main/kotlin/com/hellokopringboot/demo/api/v1/StockParseController.kt
+++ src/main/kotlin/com/hellokopringboot/demo/api/v1/StockParseController.kt
@@ -1,5 +1,6 @@
 package com.hellokopringboot.demo.api.v1
 
+import com.hellokopringboot.demo.dto.ResponseDataDto
 import com.hellokopringboot.demo.dto.YahooFinanceDto
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
@@ -22,14 +23,16 @@ class StockParseController (
         @RequestParam("ticker", defaultValue = "005930.KS") ticker: String,
         @RequestParam("interval", defaultValue = "1d") interval: String,
         @RequestParam("range", defaultValue = "5d") range: String,
-    ): String {
+    ): ResponseDataDto {
         // YahooFinance Documentation: https://syncwith.com/yahoo-finance/yahoo-finance-api
 
         val yahooFinanceURI = "https://query1.finance.yahoo.com/v8/finance/chart/$ticker?interval=$interval&range=$range"
         val response = restTemplate.getForObject<YahooFinanceDto>(yahooFinanceURI)
 
+        val timestamp = response.chart?.result?.get(0)?.timestamp
+        val quote = response.chart?.result?.get(0)?.indicators?.quote
 
-        return "1"
+        return ResponseDataDto(timestamp = timestamp, indicators = ResponseDataDto.Indicators(quote))
     }
 
 }
